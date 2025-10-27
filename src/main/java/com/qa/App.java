d35fa9d596d18576edfb4d04c6b2e7bee00a13f6
Taran Che <taranche@rocketmail.com>
2025-10-27 15:09:31 +0000

    CREATE: Added adnans name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Taran"));
+        System.out.println(sayHelloToSomeone("Adnan"));
         System.out.println(sayGoodbye());
     }
 
